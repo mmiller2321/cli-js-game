a4727a4e0781ddbfe2d44f4d13b81e5740ed7ba9
Micah Miller <mimiller12345@gmail.com>
2022-01-27 11:58:14 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import chalk from "chalk";
+// import inquirer from "inquirer";
+// import gradient from "gradient-string";
+// import chalkAnimation from "chalk-animation";
+// import figlet from "figlet";
+// import { createSpinner } from "nano-spinners"; 
+
+let playerName;
+
+async function welcome() {
+    const rainbowTitle = chalkAnimation.rainbow(
+        'Who Wants to be a JavaScript Millionaire? \n'
+    );
+}
+
+
+console.log(chalk.bgGreen('hi mom'));
+//console.log(chalk.bgRed('hi dad'));
